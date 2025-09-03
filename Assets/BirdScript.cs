63b9c8734a01c4c3a688496317e2d58eea23c057
Sudipta Bhowmick <sudipta.bhowmick@valmetpartners.com>
2025-09-03 18:42:34 +0600

    Implemented Spawning objects and, Modified Logic and UI

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidBody;
+    public float flapStrength;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,9 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidBody.linearVelocity = Vector2.up * flapStrength;
+        }
     }
 }
