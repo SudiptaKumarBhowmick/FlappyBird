260d45e60eb90dee096cea051900a797bc7f2c92
SudiptaKumarBhowmick <sudiptakumar.shuvo@gmail.com>
2025-09-06 22:57:58 +0600

    Modified the pipe prefab scene and game over for bird out of the screen

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -20,6 +20,12 @@ public class BirdScript : MonoBehaviour
         {
             myRigidBody.linearVelocity = Vector2.up * flapStrength;
         }
+
+        if (transform.position.y < -15)
+        {
+            logic.gameOver();
+            birdIsAlive = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
